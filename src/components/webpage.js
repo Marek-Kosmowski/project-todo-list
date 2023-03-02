5acdb5c9588a0737bc28a7f8cc1be08f3e3abd6d
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-02 21:27:26 +0100

    Create webpage.js in components directory and delete unnecessary code from index.js

---
+++ src/components/webpage.js
@@ -0,0 +1,6 @@
+import '../styles/style.css'
+import logo from '../assets/todo-logo.png'
+
+const logoImage = document.querySelector('.header-logo');
+
+logoImage.src = logo;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import './styles/style.css'
-import logo from './assets/todo-logo.png'
-
-const logoImage = document.querySelector('.header-logo');
-
-logoImage.src = logo;
+import webpage from './components/webpage'
