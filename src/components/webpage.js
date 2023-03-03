da20b7afe02d883acbc91fc2e024d6c25971aec1
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-03 19:40:23 +0100

    Add styles to buttons on the page

--- src/components/webpage.js
+++ src/components/webpage.js
@@ -1,13 +1,15 @@
 import '../styles/style.css'
 import logo from '../assets/todo-logo.png'
 import plusSign from '../assets/plus.svg'
-
-const modal = document.querySelector('.modal');
+import removeSign from '../assets/remove.svg'
+// const modal = document.querySelector('.modal');
 const logoImage = document.querySelector('.header-logo');
 logoImage.src = logo;
 const crateProjectBtn = document.querySelector('.project-create-button');
 crateProjectBtn.src = plusSign;
+const removeSymbol = document.querySelector('.project-remove-button');
+removeSymbol.src = removeSign;
 
-crateProjectBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
-})
+// crateProjectBtn.addEventListener('click', () => {
+//     modal.style.display = 'block';
+// })
