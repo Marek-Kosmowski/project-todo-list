e512a6bbf91337c737d802cd99307e84f70fc817
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-03 18:50:20 +0100

    Create modal form for creating a new project, add style to buttons on the page

--- src/components/webpage.js
+++ src/components/webpage.js
@@ -1,5 +1,13 @@
 import '../styles/style.css'
 import logo from '../assets/todo-logo.png'
+import plusSign from '../assets/plus.svg'
 
+const modal = document.querySelector('.modal');
 const logoImage = document.querySelector('.header-logo');
 logoImage.src = logo;
+const crateProjectBtn = document.querySelector('.project-create-button');
+crateProjectBtn.src = plusSign;
+
+crateProjectBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
